8be36bb8cf90cab9e16bfb4334fcee4984fc4cbc
ishanjain1502 <ishanjain1502@gmail.com>
2024-12-08 17:27:40 +0530

    writing rollbacks

--- decisionReasons.md
+++ decisionReasons.md
@@ -2,3 +2,9 @@
 
 ### For loggin was going to impliment a watch on DB, to  record all transactions, but then rejected the idea.
 > Reason was simple, I am expecting users to have an active service, in that case, if I record all transaction then my migration related transactions will get lost in between total transactions, then rollback purpose will be lost
+
+## For Undoing Migration
+> There are multiple ways that are bombarding my mind, and I cant think of anyone particular, so will start with first principle mechanishm.<br/><br/>
+> Will create a clone of required collections before migration in a saperate file and a clone of collection of required collection after migration with some migration versioning, so that it can be used for reverting back.<br/><br/>
+> A edgecase that came in my mind can be, when migration is used to create a new collection, then in that case, umdiong will require us to delete the newly created collection, which cant be done by overwriting the existing collections.<br/><br/>
+> Will think a little more about it.
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const {writeLog} = require('./logger');
 
+const fs = require('fs');
+
 const processMigration = async ({ uri, options={}} , primaryCollection ,arrayOfCOllectionsRequired, callbackFunc, {limitOptions=1}) => {
     try{
 
@@ -9,7 +11,20 @@ const processMigration = async ({ uri, options={}} , primaryCollection ,arrayOfC
         const dbDataPointer = getData({ limit: 100 }, primaryCollection);
 
         // write log to initiate migration
-        writeLog('initiate_migration');
+        // writeLog('initiate_migration');
+        // const csv = require('csv-writer').createObjectCsvWriter({
+        //     path: 'primaryCollectionClone.csv',
+        //     header: [
+        //         {id: '_id', title: '_id'},
+        //         {id: 'createdAt', title: 'createdAt'},
+        //         {id: 'updatedAt', title: 'updatedAt'}
+        //         // Add more fields as needed
+        //     ]
+        // });
+
+        const cloneData = await primaryCollection.find({}).lean();
+        await csv.writeRecords(cloneData);
+        console.log('Primary collection clone saved to primaryCollectionClone.csv');
 
         while (true) {
             let data = await dbDataPointer.next().value;
@@ -48,14 +63,47 @@ function* getData(options = { limit: 100 }, primaryCollection) {
     }
 }
 
-async function performOperation(data, arrayOfCOllectionsRequired, callbackFunc ) {
+async function performOperation(data, arrayOfCOllectionsRequired, callbackFunc, primaryCollection) {
     try{
 
-       await callbackFunc(data, ...arrayOfCOllectionsRequired, writeLog);
+       await callbackFunc(data, ...arrayOfCOllectionsRequired, writeLog, primaryCollection);
 
     }catch(err){
         console.log(err);
     }
 }
 
-module.exports = processMigration;
+
+const processRollback = async({ uri, options={}} , primaryCollection ,arrayOfCOllectionsRequired, callbackFunc, {limitOptions=1}) => {
+        try{
+
+        await mongoose.connect(uri, options);
+        console.log('Connected to the database');
+        const dbDataPointer = getData({ limit: 100 }, primaryCollection);
+
+        // write log to initiate rollback
+        writeLog('initiate_Rollback');
+
+        while (true) {
+            let data = await dbDataPointer.next().value;
+            if (!data || data.length == 0) {
+                console.log("Empty data");
+                break;
+            }
+            await performOperation(data, arrayOfCOllectionsRequired, callbackFunc, primaryCollection );
+        }
+
+        writeLog('rollback_complete');
+        // Close the connection
+        mongoose.connection.close();
+
+        writeLog('close_connection');
+
+        process.exit(0);
+
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = {processMigration, processRollback};
